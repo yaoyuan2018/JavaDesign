0294f5dabfa3412a098c84231f008dcff1b21244
yy <yy494818027@163.com>
2018-11-29 08:31:26 +0800

    Java设计模式：迭代器模式

---
+++ src/IteratorPattern/IProjectIterator.java
@@ -0,0 +1,13 @@
+package IteratorPattern;
+
+import java.util.Iterator;
+
+/**
+ * @Author: Y_uan
+ * @Date: 2018/11/27 14:01
+ * @mail: yy494818027@163.com
+ * 定义个Iterator接口
+ */
+@SuppressWarnings("all")
+public interface IProjectIterator extends Iterator {
+}
